cf0501ae9ed2685b758fba934ca5c1f3e8a7a18f
Carlos Peña <charly_13_eduard@hotmail.com>
2015-07-26 12:56:15 -0500

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -103,4 +103,11 @@ exports.update = function(req, res) {
       }   // Redirección HTTP a lista de preguntas (URL relativo)
     }
   );
-};  
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  	req.quiz.destroy().then( function() {
+    	res.redirect('/quizes');
+  	}).catch(function(error){next(error)});
+};   
